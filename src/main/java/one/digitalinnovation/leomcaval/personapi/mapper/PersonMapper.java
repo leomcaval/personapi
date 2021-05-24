c0478ee89753f0a6d8a758cad1a81ad646d3f5d3
leomcaval <leonardomachado3112@gmail.com>
2021-05-24 13:13:28 -0300

    finshed Parte 8

---
+++ src/main/java/one/digitalinnovation/leomcaval/personapi/mapper/PersonMapper.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.leomcaval.personapi.mapper;
+
+import one.digitalinnovation.leomcaval.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.leomcaval.personapi.entities.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+
+public interface PersonMapper {
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    PersonDTO toDTO(Person person);
+}
--- src/main/java/one/digitalinnovation/leomcaval/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/leomcaval/personapi/service/PersonService.java
@@ -3,15 +3,20 @@ package one.digitalinnovation.leomcaval.personapi.service;
 import one.digitalinnovation.leomcaval.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.leomcaval.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.leomcaval.personapi.entities.Person;
+import one.digitalinnovation.leomcaval.personapi.mapper.PersonMapper;
 import one.digitalinnovation.leomcaval.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository) {
@@ -19,19 +24,19 @@ public class PersonService {
     }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO){
+        Person personToSave = personMapper.toModel(personDTO);
 
-        Person personToSave = Person.builder()
-                .firstName(personDTO.getFirstName())
-                .lastName(personDTO.getLastName())
-                .birthDate(personDTO.getBirthDate())
-                .phones(personDTO.getPhones())
-                .build();
-
-        Person savedPerson = personRepository.save(personDTO);
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID" + savedPerson.getId())
                 .build();
     }
 
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
 }
