6017cea9802d7025a328cc47b3efc12c62b12d23
leomcaval <leonardomachado3112@gmail.com>
2021-05-23 21:50:55 -0300

    my first commit

---
+++ src/main/java/one/digitalinnovation/leomcaval/personapi/controllers/PersonController.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.leomcaval.personapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/person")
+public class PersonController {
+
+    @GetMapping
+    public String getBook(){
+        return "API Test!";
+    }
+
+
+}
